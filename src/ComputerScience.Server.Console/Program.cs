5be956fc37d5ff511bd9dd1b8571f32aa6c53000
Justin Ley <inkysigma@outlook.com>
2016-05-28 00:28:24 -0700

    Moved files around and tested a new project to load the webserver

---
+++ src/ComputerScience.Server.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComputerScience.Server.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var webServer = new Process {StartInfo = new ProcessStartInfo("dotnet", "run ComputerScience.Server.Web")};
+            webServer.StartInfo.CreateNoWindow = false;
+            webServer.Start();
+            System.Console.WriteLine("Web server has started.");
+            System.Console.ReadKey();
+            webServer.Dispose();
+        }
+    }
+}
