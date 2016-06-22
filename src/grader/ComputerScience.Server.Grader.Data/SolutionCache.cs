7933eeb38d6e999c6c0766eeec52af4aa289dafb
Justin Ley <inkysigma@outlook.com>
2016-06-22 15:44:50 -0700

    Started the SolutionCache

---
+++ src/grader/ComputerScience.Server.Grader.Data/SolutionCache.cs
@@ -0,0 +1,21 @@
+﻿using StackExchange.Redis;
+using ComputerScience.Server.Commons;
+
+namespace ComputerScience.Server.Grader.Data
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class SolutionCache
+    {
+        public IDatabase Database { get; }
+        
+        public SolutionCache(IDatabase database)
+        {
+            Database = database;
+        }
+
+        public async Task<Solution> Fetch(){
+            Database.
+        }
+    }
+}
