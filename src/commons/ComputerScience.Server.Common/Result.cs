751386677ddfb28d8a93574b42d4eefef5591086
Justin Ley <inkysigma@outlook.com>
2016-06-22 22:43:46 -0700

    Added Result model

---
+++ src/commons/ComputerScience.Server.Common/Result.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComputerScience.Server.Common
+{
+    public class Result
+    {
+        public string Id { get; set; }
+        public TestCase[] TestCases { get; set; }
+    }
+
+    public enum TestCase
+    {
+        TimeOut,
+        Error,
+        Failure,
+        Success
+    }
+}
--- src/grader/ComputerScience.Server.Grader/Grader.cs
+++ src/grader/ComputerScience.Server.Grader/Grader.cs
@@ -5,12 +5,24 @@ namespace ComputerScience.Server.Grader
 {
     public class Grader
     {
-        public ISolutionCache<Solution> Cache { get; }
+        public ISolutionCache<Solution> SolutionCache { get; }
+        public IProblemSet<Problem> ProblemSet { get; }
+        public string Directory { get; }
 
-        
-        public void Start()
+        public Grader(ISolutionCache<Solution> solutionCache, IProblemSet<Problem> problemSet, string directory)
         {
-            
+            SolutionCache = solutionCache;
+            ProblemSet = problemSet;
+            Directory = directory;
+        }
+        
+        public async void Start()
+        {
+            while (Program.IsRunning)
+            {
+                var solution = await SolutionCache.Fetch();
+
+            }
         }
     }
 }
