d0034a70d83ed2b639c6a0db2bbcdaa45601a28b
Justin Ley <inkysigma@outlook.com>
2016-06-22 23:55:19 -0700

    Added python compiler and modified Grader accordingly

---
+++ src/grader/ComputerScience.Server.Grader/Compiler/PythonCompiler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ComputerScience.Server.Common;
+
+namespace ComputerScience.Server.Grader.Compiler
+{
+    public class PythonCompiler : ICompiler
+    {
+        public CompilerResult Compile(Solution solution, string directory)
+        {
+            if (!File.Exists(Path.Combine(solution.FileLocation, solution.File)))
+                CompilerResult.Fail("Something has gone wrong.Please notify the site administrators.");
+            File.Copy(Path.Combine(solution.FileLocation, solution.File), Path.Combine(directory, solution.File));
+            return CompilerResult.Succeed();
+        }
+    }
+}
--- src/grader/ComputerScience.Server.Grader/Grader.cs
+++ src/grader/ComputerScience.Server.Grader/Grader.cs
@@ -1,4 +1,6 @@
-﻿using ComputerScience.Server.Common;
+﻿using System.Collections.Generic;
+using ComputerScience.Server.Common;
+using ComputerScience.Server.Grader.Compiler;
 using ComputerScience.Server.Grader.Data;
 
 namespace ComputerScience.Server.Grader
@@ -8,12 +10,15 @@ namespace ComputerScience.Server.Grader
         public ISolutionCache<Solution> SolutionCache { get; }
         public IProblemSet<Problem> ProblemSet { get; }
 
+        public Dictionary<SolutionType, ICompiler> Compilers { get; }
+
         public string Directory { get; }
 
-        public Grader(ISolutionCache<Solution> solutionCache, IProblemSet<Problem> problemSet, string directory)
+        public Grader(ISolutionCache<Solution> solutionCache, IProblemSet<Problem> problemSet, Dictionary<SolutionType, ICompiler> compilers, string directory)
         {
             SolutionCache = solutionCache;
             ProblemSet = problemSet;
+            Compilers = compilers;
             Directory = directory;
         }
         
@@ -22,7 +27,7 @@ namespace ComputerScience.Server.Grader
             while (Program.IsRunning)
             {
                 var solution = await SolutionCache.Fetch();
-                
+                Compilers[solution.SolutionType].Compile(solution, Directory);
             }
         }
     }
